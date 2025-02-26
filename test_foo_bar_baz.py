e4e5d0c9028e995766f440b7e27307490fb5b915
jvogel <joevogel501@gmail.com>
2025-02-26 13:30:06 -0800

    cleaned up comments

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 def testAgainstCorrect():
 	from foo_bar_baz import foo_bar_baz
 	def correct_foo_bar_baz(n: int) -> str:
@@ -23,52 +22,6 @@ def testAgainstCorrect():
 
 	for i in range (1, 1000):
 		assert not foo_bar_baz(i).endswith(" "), f"foo_bar_baz({i}) ended with ' '."
-
-
-
-"""
-expected_outputs = [ # 1 - 15
-    "",
-    "1",  
-    "1 2",  
-    "1 2 Foo",  
-    "1 2 Foo 4",  
-    "1 2 Foo 4 Bar",  
-    "1 2 Foo 4 Bar Foo",  
-    "1 2 Foo 4 Bar Foo 7",  
-    "1 2 Foo 4 Bar Foo 7 8",  
-    "1 2 Foo 4 Bar Foo 7 8 Foo",  
-    "1 2 Foo 4 Bar Foo 7 8 Foo Bar", 
-    "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11",  
-    "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo",  
-    "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13",  
-    "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14",  
-    "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz",  
-]
-
-A = list(range(500))
-
-for i in range(1, len(A)):
-	if i % 5 == 0 and i % 3 == 0:
-		A[i] = "Baz"
-	elif i % 3 == 0:
-		A[i] = "Foo"
-	elif i % 5 == 0:
-		A[i] = "Bar"
-	else:
-		A[i] = str(i)
-
-def test_list_500():
-	for i in range(1, len(A)):
-		assert foo_bar_baz(i).split()[-1] == A[i], f"foo_bar_baz({i}) failed"
-
-#Add testcases Here
-def test_regular_numbers():
-    # positive tests
-
-    for i in range(0, len(expected_outputs)):
-        assert foo_bar_baz(i) == expected_outputs[i], f"foo_bar_baz({i}) failed"
-"""
 
 def test_negative_numbers():
 	from foo_bar_baz import foo_bar_baz
