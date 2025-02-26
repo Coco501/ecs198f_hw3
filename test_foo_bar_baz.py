0300bcb3362ca0fc082cd45126ef085be9fe3b61
jvogel <joevogel501@gmail.com>
2025-02-26 13:28:26 -0800

    indentation fix, imported function locally in each test function

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -1,8 +1,9 @@
 import pytest
-from foo_bar_baz import foo_bar_baz
+
 
 def testAgainstCorrect():
-		def correct_foo_bar_baz(n: int) -> str:
+	from foo_bar_baz import foo_bar_baz
+	def correct_foo_bar_baz(n: int) -> str:
 			return_str = ""
 			for i in range(1, n + 1, 1):
 				if (i % 3 == 0) and (i % 5 == 0):
@@ -17,11 +18,12 @@ def testAgainstCorrect():
 					return_str += " "
 			return return_str
 
-		for i in range(-100, 1000):
-			assert correct_foo_bar_baz(i) == foo_bar_baz(i)
+	for i in range(-100, 1000):
+		assert correct_foo_bar_baz(i) == foo_bar_baz(i)
 
-		for i in range (1, 1000):
-			assert not foo_bar_baz(i).endswith(" "), f"foo_bar_baz({i}) ended with ' '."
+	for i in range (1, 1000):
+		assert not foo_bar_baz(i).endswith(" "), f"foo_bar_baz({i}) ended with ' '."
+
 
 
 """
@@ -69,11 +71,13 @@ def test_regular_numbers():
 """
 
 def test_negative_numbers():
-    assert foo_bar_baz(-1253) == ""
-    assert foo_bar_baz(-5) == ""
-    assert foo_bar_baz(-0) == ""
+	from foo_bar_baz import foo_bar_baz
+	assert foo_bar_baz(-1253) == ""
+	assert foo_bar_baz(-5) == ""
+	assert foo_bar_baz(-0) == ""
 
 def test_wrong_types():
+    from foo_bar_baz import foo_bar_baz
     with pytest.raises(TypeError):
         foo_bar_baz("string type")
     with pytest.raises(TypeError):
@@ -82,6 +86,7 @@ def test_wrong_types():
         foo_bar_baz()
 
 def test_large_numbers_return():
+    from foo_bar_baz import foo_bar_baz
     assert foo_bar_baz(608106).split()[-1] == "Foo"
     assert foo_bar_baz(360916).split()[-1] == "360916"
     assert foo_bar_baz(461980).split()[-1] == "Bar"
